2c4c4ec55e9206a6be5b2096501a395440638144
NicolasMCP <nicolas.mcp@gmail.com>
2018-02-06 12:04:47 -0200

    ExportCatalog para Thunderbird

---
+++ ExportCatalog/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDat = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnFim = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnProcessar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Coloque aqui os dados a exportar";
+            // 
+            // txtDat
+            // 
+            this.txtDat.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDat.Location = new System.Drawing.Point(15, 29);
+            this.txtDat.Multiline = true;
+            this.txtDat.Name = "txtDat";
+            this.txtDat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDat.Size = new System.Drawing.Size(793, 596);
+            this.txtDat.TabIndex = 1;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(608, 631);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(97, 31);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "&Exportar";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(402, 631);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(97, 31);
+            this.btnImport.TabIndex = 3;
+            this.btnImport.Text = "&Importar";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnFim
+            // 
+            this.btnFim.Location = new System.Drawing.Point(711, 631);
+            this.btnFim.Name = "btnFim";
+            this.btnFim.Size = new System.Drawing.Size(97, 31);
+            this.btnFim.TabIndex = 4;
+            this.btnFim.Text = "&Fim";
+            this.btnFim.UseVisualStyleBackColor = true;
+            this.btnFim.Click += new System.EventHandler(this.btnFim_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // btnProcessar
+            // 
+            this.btnProcessar.Location = new System.Drawing.Point(505, 631);
+            this.btnProcessar.Name = "btnProcessar";
+            this.btnProcessar.Size = new System.Drawing.Size(97, 31);
+            this.btnProcessar.TabIndex = 5;
+            this.btnProcessar.Text = "&Processar";
+            this.btnProcessar.UseVisualStyleBackColor = true;
+            this.btnProcessar.Click += new System.EventHandler(this.btnProcessar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 674);
+            this.Controls.Add(this.btnProcessar);
+            this.Controls.Add(this.btnFim);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.txtDat);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Exportar Catalogo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDat;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnFim;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnProcessar;
+    }
+}
+
---
+++ ExportCatalog/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Autor: Herley Nicolas Ramos Sanchez
+ * email: nicolas.mcp@gmail.com
+ * licença:  GNU GPL
+ * Objetivo: facilitar a importação de arquivos "copiados do cabeçalho de um web-mail" para
+ *           criar um catálogo de endereços no Thunderbird
+ */
+
+namespace WindowsFormsApplication2 {
+
+    public partial class Form1: Form {
+
+        public Form1() {
+
+            InitializeComponent();
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e) {
+
+            string s = txtDat.Text.Trim();
+            if(s.Length > 0) {
+                saveFileDialog1.Filter = "Valores Separados por Vírgula (*.csv)|*.csv";
+                saveFileDialog1.DefaultExt = "csv";
+                saveFileDialog1.AddExtension = true;
+                saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                saveFileDialog1.ShowDialog();
+            } else {
+                MessageBox.Show("Para exportar você deve ter conteúdo relevante!", "Aviso!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e) {
+
+            string caminho = saveFileDialog1.FileName;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(txtDat.Text);
+            sb.Replace("\r","");
+            
+            File.WriteAllText(caminho, sb.ToString());
+        }
+
+        private void btnFim_Click(object sender, EventArgs e) {
+
+            this.Close();
+
+        }
+
+        private void btnImport_Click(object sender, EventArgs e) {
+
+            openFileDialog1.Filter = "Arquivos de Texto (*.txt)|*.txt";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.AddExtension = true;
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog1.ShowDialog();
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e) {
+
+            btnImport.Enabled = false;
+            string caminho = openFileDialog1.FileName;
+            txtDat.Text = File.ReadAllText(caminho);
+
+        }
+
+        private void btnProcessar_Click(object sender, EventArgs e) {
+
+            string s;
+            int menorQue;
+            int arroba;
+            int espaco;
+            string umaLinha;
+            string nome;
+
+            btnProcessar.Enabled = false;
+            btnImport.Enabled = false;
+
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sbFinal = new StringBuilder();
+
+            sb.Append(txtDat.Text);
+            s = CortesLinha(sb);
+
+            if(s.Substring(0, 13) != "Primeiro nome,") {
+
+                sbFinal.Append("Primeiro nome,Sobrenome,Apresentar como,Apelido,E-mail primário,E-mail secundário,Nome de tela,Telefone comercial,Telefone residencial,Número do fax,Número do pager,Número celular,Endereço,Endereço 2,Cidade,Estado,CEP,País,Endereço de trabalho,Endereço de trabalho 2,Cidade de trabalho,Estado de trabalho,CEP de trabalho,País de trabalho,Cargo,Setor,Empresa,Página da web 1,Página da web 2,Ano de nascimento,Mês de nascimento,Nascimento,Personalizado 1,Personalizado 2,Personalizado 3,Personalizado 4,Observações,\n");
+            }
+
+            do {
+                umaLinha = ExtraiUmaLinha(s);
+
+                if((umaLinha.Length) < s.Length) {
+                    s = s.Remove(0, umaLinha.Length);
+                } else {
+                    s = "";
+                }
+                
+                menorQue = umaLinha.IndexOf("<");
+                arroba = umaLinha.IndexOf("@");
+                espaco = umaLinha.IndexOf(" ");
+
+                if(menorQue == -1) {
+
+                    menorQue = int.MaxValue;
+                }
+
+                if(espaco == -1) {
+
+                    espaco = int.MaxValue;
+                }
+
+                if(arroba == -1) {
+
+                    MessageBox.Show("Há um erro no formato dos dados", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if(arroba < menorQue) {
+
+                    if(espaco < arroba) {
+
+                        MessageBox.Show("Há um erro no formato dos dados", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+
+                    } else {
+
+                        nome = umaLinha.Substring(0, arroba);
+                        sb.Append(nome + ",," + nome + ",," + umaLinha);
+                        sb.Replace("|", ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
+                        sbFinal.Append(sb.ToString());
+                        sb.Clear();
+                        sbFinal.Replace("\n", "\r\n");
+                        txtDat.Text = sbFinal.ToString();
+                    }
+
+                } else {
+
+                    if(espaco == (menorQue - 1)){
+
+                        nome = umaLinha.Substring(0, espaco);
+                        sb.Append(nome + ",," + umaLinha);
+                        sb.Replace(" <", ",,");
+                        sb.Replace("|", ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,");
+                        sbFinal.Append(sb.ToString());
+                        sb.Clear();
+                        sbFinal.Replace("\n", "\r\n");
+                        txtDat.Text = sbFinal.ToString();
+                    }
+
+                }
+
+            } while(s.Length > 3);
+
+        }
+
+        private string ExtraiUmaLinha(string s) {
+
+            s = s.Substring(0, s.IndexOf("|\n") + 2);
+            return s;
+        }
+
+        private string CortesLinha(StringBuilder sb) {
+
+            int final;
+            string s;
+            sb.Replace(">, ", "|\n");
+            sb.Replace(", ", "|\n");
+            s = sb.ToString();
+            sb.Clear();
+
+            final = s.Length - 2;
+            if(!s.Substring(final, 2).Equals("|\n")) {
+                if(s.Substring(final + 1, 1).Equals(">")|| s.Substring(final + 1, 1).Equals(",")) {
+                    s = s.Remove(final + 1, 1);
+                }
+                s = s + "|\n";
+            }
+            return s;
+        }
+    }
+}
